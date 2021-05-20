8c85bbac19d777af822adefefeec895e885d2949
Haresh-vijeyan <83931619+Haresh-vijeyan@users.noreply.github.com>
2021-05-21 00:40:17 +0800

    Update SystemMaintenance.sh

--- SystemMaintenance.sh
+++ SystemMaintenance.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+#echo --------------------
+#date
+#echo --------------------
+#echo
+passno=0
+failno=0
 
 echo 6.1 System File Permissions
 
@@ -16,8 +22,10 @@ pcheck=$(stat --format="%A" /etc/passwd)
 if [[ $pcheck != "-rw-r--r--" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+	passno=$(($passno + 1))
 fi
 
 echo 6.1.3  Ensure permissions on /etc/shadow are configured
@@ -26,8 +34,11 @@ pecheck=$(stat --format="%A" /etc/shadow)
 if [[ $pecheck != "-rw-r-----" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.4  Ensure permissions on /etc/group are configured
@@ -36,8 +47,11 @@ percheck=$(stat --format="%A" /etc/group)
 if [[ $percheck != "-rw-r--r--" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.5  Ensure permissions on /etc/gshadow are configured
@@ -46,8 +60,11 @@ permcheck=$(stat --format="%A" /etc/gshadow)
 if [[ $permcheck != "-rw-r-----" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.6  Ensure permissions on /etc/passwd- are configured
@@ -56,8 +73,11 @@ permicheck=$(stat --format="%A" /etc/passwd-)
 if [[ $permicheck != "-rw-------" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.7  Ensure permissions on /etc/shadow- are configured
@@ -66,8 +86,11 @@ permischeck=$(stat --format="%A" /etc/shadow-)
 if [[ $permischeck != "-rw-------" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.8  Ensure permissions on /etc/group- are configured
@@ -76,8 +99,11 @@ permisscheck=$(stat --format="%A" /etc/group-)
 if [[ $permisscheck != "-rw-------" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.9 Ensure permissions on /etc/gshadow- are configured
@@ -86,8 +112,11 @@ permissicheck=$(stat --format="%A" /etc/gshadow-)
 if [[ $permissicheck != "-rw-r-----" ]]
 then
 	echo "Fail: Permisions are not set" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: Permisions are set"
+		passno=$(($passno + 1))
+
 fi
 
 
@@ -97,8 +126,11 @@ echeck=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -
 if [[ $echeck != "" ]]
 then
 	echo "Fail: world writable files exist" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: No world writable files exist"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.11 Ensure no unowned files or directories exist
@@ -107,8 +139,11 @@ encheck=$(df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' 
 if [[ $encheck != "" ]]
 then
 	echo "Fail: unowned files or directories exist" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: No unowned files or directories exist"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.12 Ensure no ungrouped files or directories exist
@@ -117,8 +152,11 @@ enscheck=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}'
 if [[ $enscheck != "" ]]
 then
 	echo "Fail: ungrouped files or directories exist" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: No ungrouped files or directories exist"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.1.13 Audit SUID executables
@@ -135,8 +173,11 @@ pcheck=$(awk -F: '($2 == "" ) { print $1 " does not have a password "}' /etc/sha
 if [[ $pcheck != "" ]]
 then
 	echo "Fail: password feilds are empty" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: password feilds are not empty"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.2.2 Ensure no legacy "+" entries exist in /etc/passwd
@@ -145,8 +186,11 @@ lcheck=$(grep '^\+:' /etc/passwd)
 if [[ $lcheck != "" ]]
 then
 	echo "Fail: legacy "+" entries exist in /etc/passwd" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: no legacy "+" entries exist in /etc/passwd"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.2.4 Ensure no legacy "+" entries exist in /etc/shadow
@@ -155,8 +199,11 @@ lecheck=$(grep '^\+:' /etc/shadow)
 if [[ $lecheck != "" ]]
 then
 	echo "Fail: legacy "+" entries exist in /etc/shadow" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: no legacy "+" entries exist in /etc/shadow"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.2.5 Ensure no legacy "+" entries exist in /etc/group
@@ -165,8 +212,11 @@ legcheck=$(grep '^\+:' /etc/group)
 if [[ $legcheck != "" ]]
 then
 	echo "Fail: legacy "+" entries exist in /etc/group" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: no legacy "+" entries exist in /etc/group"
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.2.6 Ensure root is the only UID 0 account
@@ -174,9 +224,12 @@ echo 6.2.6 Ensure root is the only UID 0 account
 rcheck=$(awk -F: '($3 == 0) { print $1 }' /etc/passwd)
 if [[ $rcheck != "root" ]]
 then
-	echo "Pass: root is the only UID 0 account" 
+	echo "Fail: root is the only UID 0 account" 
+	failno=$(($failno + 1))
 else 
 	echo "Pass: root is the only UID 0 account" 
+		passno=$(($passno + 1))
+
 fi
 
 echo 6.2.8 Ensure users own their home directories
@@ -189,8 +242,9 @@ grep -E -v '^(halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "'"$(which nolo
         if [ "$owner" != "$user" ]; then
             echo "The home directory \"$dir\" of user $user is owned by $owner."
         fi
+		
     fi
-	echo (If no files are output PASS) 
+	echo "If no files are output PASS"
 done
 
 
@@ -212,5 +266,8 @@ grep -E -v '^(halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "'"$(which nolo
             fi
         done
     fi
-	echo (If no files are output PASS) 
+	echo "If no files are output PASS"
 done
+
+echo 'NUMBER OF PASSES:	'$passno''
+echo 'NUMBER OF FAILED:	'$failno''
