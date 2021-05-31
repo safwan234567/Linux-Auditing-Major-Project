f581acd6cfb65158a6724e4ddefbe44bd2276819
Mzimmy <83450140+Mzimmy@users.noreply.github.com>
2021-06-01 00:04:50 +0800

    Update level2server.sh

--- level2server.sh
+++ level2server.sh
@@ -83,11 +83,11 @@ echo 3.3.1 Ensure DCCP is disabled
 #3.3.1
 if ! modprobe -n -v dccp 2> /dev/null | tail -1 | grep -q 'install /bin/true'
         then
-                echo -e "${RED}Fail:    DCCP is not disabled
+                echo -e "${RED}Fail:	DCCP is not disabled
                 ${ENDCOLOR}"
                 failno=$(($failno + 1))
         else
-                echo -e "${GREEN}Pass:  DCCP is disabled
+                echo -e "${GREEN}Pass:	DCCP is disabled
                 ${ENDCOLOR}"
                 passno=$(($passno + 1))
         fi
@@ -97,11 +97,11 @@ echo 3.3.2 Ensure SCTP is disabled
 #3.3.2
 if ! modprobe -n -v sctp 2> /dev/null | tail -1 | grep -q 'install /bin/true'
         then
-                echo -e "${RED}Fail:    SCTP is not disabled
+                echo -e "${RED}Fail:	SCTP is not disabled
                 ${ENDCOLOR}"
                 failno=$(($failno + 1))
         else
-                echo -e "${GREEN}Pass:  SCTP is disabled
+                echo -e "${GREEN}Pass:	SCTP is disabled
                 ${ENDCOLOR}"
                 passno=$(($passno + 1))
         fi
@@ -111,11 +111,11 @@ echo 3.3.3 Ensure RDS is disabled
 #3.3.3
 if ! modprobe -n -v rds 2> /dev/null | tail -1 | grep -q 'install /bin/true'
         then
-                echo -e "${RED}Fail:    RDS is not disabled
+                echo -e "${RED}Fail:	RDS is not disabled
                 ${ENDCOLOR}"
                 failno=$(($failno + 1))
         else
-                echo -e "${GREEN}Pass:  RDS is disabled
+                echo -e "${GREEN}Pass:	RDS is disabled
                 ${ENDCOLOR}"
                 passno=$(($passno + 1))
         fi
@@ -125,11 +125,11 @@ echo 3.3.4 Ensure TIPC is disabled
 #3.3.4
 if ! modprobe -n -v tipc 2> /dev/null | tail -1 | grep -q 'install /bin/true'
         then
-                echo -e "${RED}Fail:    TIPC is not disabled
+                echo -e "${RED}Fail:	TIPC is not disabled
                 ${ENDCOLOR}"
                 failno=$(($failno + 1))
         else
-                echo -e "${GREEN}Pass:  TIPC is disabled
+                echo -e "${GREEN}Pass:	TIPC is disabled
                 ${ENDCOLOR}"
                 passno=$(($passno + 1))
         fi
@@ -140,11 +140,11 @@ echo 3.6 Disable IPv6
 ipv6disable=$(grep -E "^\s*kernelopts=(\S+\s+)*ipv6\.disable=1\b\s*(\S+\s*)*$" /boot/grub2/grubenv)
         if [[ -z $ipv6disable ]]
         then
-                echo -e "${RED}Fail:    IPv6 is not disabled
+                echo -e "${RED}Fail:	IPv6 is not disabled
                 ${ENDCOLOR}"
                 failno=$(($failno + 1))
         else
-                echo -e "${GREEN}Pass:  IPv6 is disabled
+                echo -e "${GREEN}Pass:	IPv6 is disabled
                 ${ENDCOLOR}"
                 passno=$(($passno + 1))
         fi
